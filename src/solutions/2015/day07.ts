fdd2cf6fc60eea53d7655b1aaf1b6d87d4273048
Peter Aleksander Bizjak <peter.aleksander@bizjak.dev>
2025-10-16 06:19:43 +0200

    Update dependencies

--- src/solutions/2015/day07.ts
+++ src/solutions/2015/day07.ts
@@ -1,11 +1,11 @@
 import type { Solution } from "../../common";
 
 export class Day07Year2015 implements Solution {
-    first(input: string): number {
-        return -1;
-    }
+	first(input: string): number {
+		return -1;
+	}
 
-    second(input: string): number {
-        return -1;
-    }
+	second(input: string): number {
+		return -1;
+	}
 }
